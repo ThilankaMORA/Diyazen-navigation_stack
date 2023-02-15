e23abeed0bfb924e25047210bb966ed469d22533
ThilankaMORA <wickramawmtb.19@uom.lk>
2023-02-15 16:56:15 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/thilanka/udemy_ws/src/explorer_bot
+# Build directory: /home/thilanka/udemy_ws/src/explorer_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
